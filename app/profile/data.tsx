2f7c086f4d218ba613b25e6980c3e1b894a1e480
Saurabh Kulkarni <saurabhk201@gmail.com>
2023-11-24 02:38:56 +0530

    added dashboard to user prfile for public access

---
+++ app/profile/data.tsx
@@ -0,0 +1,143 @@
+export const members = [
+    {
+      "id": "1",
+      "name": "Amit",
+      "email": "amit@processwise.ai",
+      "status": "In active",
+      "role": "IT Admin"
+    },
+    {
+      "id": "2",
+      "name": "Neha",
+      "email": "neha@processwise.ai",
+      "status": "active",
+      "role": "IT Admin"
+    },
+    {
+      "id": "3",
+      "name": "Priya",
+      "email": "priya@processwise.ai",
+      "status": "active",
+      "role": "IT Admin"
+    },
+    {
+      "id": "4",
+      "name": "Rajesh",
+      "email": "rajesh@processwise.ai",
+      "status": "active",
+      "role": "Approver"
+    },
+    {
+      "id": "5",
+      "name": "Sita",
+      "email": "sita@processwise.ai",
+      "status": "active",
+      "role": "Approver"
+    },
+    {
+      "id": "6",
+      "name": "Arjun",
+      "email": "arjun@processwise.ai",
+      "status": "active",
+      "role": "Approver"
+    },
+    {
+      "id": "7",
+      "name": "Kavita",
+      "email": "kavita@processwise.ai",
+      "status": "In active",
+      "role": "End User"
+    },
+    {
+      "id": "8",
+      "name": "Amit",
+      "email": "manish_amit@processwise.ai",
+      "status": "active",
+      "role": "End User"
+    },
+    {
+      "id": "9",
+      "name": "Pooja",
+      "email": "pooja@processwise.ai",
+      "status": "active",
+      "role": "End User"
+    },
+    {
+      "id": "10",
+      "name": "Sunil",
+      "email": "sunil@processwise.ai",
+      "status": "active",
+      "role": "End User"
+    },
+    {
+      "id": "11",
+      "name": "Hans",
+      "email": "hans@processwise.ai",
+      "status": "active",
+      "role": "End User"
+    },
+    {
+      "id": "12",
+      "name": "Greta",
+      "email": "greta@processwise.ai",
+      "status": "active",
+      "role": "End User"
+    },
+    {
+      "id": "13",
+      "name": "Klaus",
+      "email": "klaus@processwise.ai",
+      "status": "active",
+      "role": "End User"
+    },
+    {
+      "id": "14",
+      "name": "Lena",
+      "email": "lena@processwise.ai",
+      "status": "active",
+      "role": "End User"
+    },
+    {
+      "id": "15",
+      "name": "Friedrich",
+      "email": "friedrich@processwise.ai",
+      "status": "active",
+      "role": "End User"
+    },
+    {
+      "id": "16",
+      "name": "Marta",
+      "email": "marta@processwise.ai",
+      "status": "active",
+      "role": "End User"
+    },
+    {
+      "id": "17",
+      "name": "Heinrich",
+      "email": "heinrich@processwise.ai",
+      "status": "active",
+      "role": "End User"
+    },
+    {
+      "id": "18",
+      "name": "Ingrid",
+      "email": "ingrid@processwise.ai",
+      "status": "active",
+      "role": "End User"
+    },
+    {
+      "id": "19",
+      "name": "Wolfgang",
+      "email": "wolfgang@processwise.ai",
+      "status": "active",
+      "role": "End User"
+    },
+    {
+      "id": "20",
+      "name": "Helga",
+      "email": "helga@processwise.ai",
+      "status": "active",
+      "role": "End User"
+    },
+    // ... (27 more End User users)
+  ]
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,3 +1,137 @@
-export default function Profile() {
-    return <p>User profile</p>
-}
+'use client'
+import React, { useState, FormEvent } from 'react';
+import { SmallAddIcon, EmailIcon } from '@chakra-ui/icons';
+import UserInput from '@/app/components/UserInputComponent';
+import UserSelect from '@/app/components/UserSelectComponent';
+import SubmitButton from '@/app/components/SubmitButtonComponent';
+import AlertComponent from '@/app/components/AlertComponent';
+import { v4 as uuidv4 } from 'uuid';
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  Container,
+  HStack,
+  Icon,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Stack,
+  Text,
+  useBreakpointValue,
+} from '@chakra-ui/react'
+import { FiSearch } from 'react-icons/fi'
+import { MemberTable } from './MemberTable'
+
+
+const RegisterForm: React.FC = () => {
+  const [formData, setFormData] = useState({ clientName: '', clientEmail: '', userRole: '' });
+  const [submitting, setSubmitting] = useState<boolean>(false);
+  const [response, setResponse] = useState<{ status: 'success' | 'error'; message: string } | null>(null);
+
+  const handleInputChange = (field: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [field]: e.target.value });
+  };
+  const handleSelectChange = (field: string) => (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFormData({ ...formData, [field]: e.target.value });
+  };
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    setSubmitting(true);
+    setResponse(null);
+
+    try {
+      const clientId = uuidv4();
+      const modelName = 'ClientDetail';
+      const userData = {
+        clientId: clientId,
+        clientName: formData.clientName,
+        clientEmail: formData.clientEmail,
+      };
+      const response = await fetch('http://localhost:7071/api/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ modelName, userData })
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log('Client registered:', data);
+      setResponse({ status: 'success', message: 'User registered successfully!' });
+    } catch (error) {
+      console.error('Error registering client:', error);
+      setResponse({ status: 'error', message: 'Failed to register user. Please try again.' });
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  if (response) {
+    return (
+      <AlertComponent
+        mainHeader={response.status === 'success' ? 'Success!' : 'Error!'}
+        subHeader={response.message}
+        status={response.status}
+        link="/dashboard/users" 
+        linkText="Return to User Registration"
+      />
+    );
+  }
+  const formFields = [
+    { id: 'user-name', icon: <SmallAddIcon/>, label: 'User Name', value: formData.clientName, onChange: handleInputChange('userName') },
+    { id: 'user-email',icon: <EmailIcon/>, label: 'User Email Address', type: 'email', value: formData.clientEmail, onChange: handleInputChange('userEmail') }
+  ];
+  const isMobile = false
+  return (
+    
+    <>
+      {/* <Box className="p-6 mx-auto">
+        <form onSubmit={handleSubmit}>
+          {formFields.map(({ id, label, type, value, onChange, icon }) => (
+            <>
+              <UserInput
+                key={id}
+                id={id}
+                type={type}
+                placeholder={`Enter ${label}`}
+                value={value}
+                onChange={onChange}
+                disabled={submitting}
+                icon={icon}
+              />
+              <div className="mb-5"></div>
+            </>
+          ))}
+              <UserSelect
+                value= {formData.userRole}
+                onChange={handleSelectChange('userRole')}
+                options={[ 'End User', 'Approver', 'IT Admin']}
+                placeholder="Select User role"
+              />
+          <SubmitButton isLoading={submitting} buttonText="Add User" />
+          
+        </form>
+      </Box> */}
+      <Box
+        bg="bg.surface"
+        boxShadow={{ base: 'none', md: 'sm' }}
+        borderRadius={{ base: 'none', md: 'lg' }}
+      >
+        <Stack spacing="5">
+       
+          <Box overflowX="auto">
+            <MemberTable />
+          </Box>
+         
+        </Stack>
+      </Box>
+    </>
+  );
+};
+
+export default RegisterForm;
+
