989a610f2080f6dfb91440ba51879a7f083e6684
Saurabh Kulkarni <saurabhk201@gmail.com>
2024-03-09 22:03:11 +0530

    updated gitignore

--- app/ui/TopNavBar.tsx
+++ app/ui/TopNavBar.tsx
@@ -31,7 +31,7 @@ const TopNav: React.FC<TopNavProps> = () => {
     <div className="flex items-center justify-between w-full">
       {/* Left-aligned logo */}
       <div className="flex items-center">
-        <Image src="/logo_large.png" width={300} height={50} alt="Logo" />
+        <Image src="/logo_large.png" width={300} height={30} alt="Logo" />
       </div>
 
       {/* Center-aligned menu items */}
