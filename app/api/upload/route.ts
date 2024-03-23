830086ff89fc8bcbb9f6084b46bf7c48419ae449
Saurabh Kulkarni <saurabhk201@gmail.com>
2024-03-23 06:51:55 +0530

    disable upload button while uploading

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,29 +1,27 @@
 import { NextResponse, NextRequest } from "next/server";
 import path from "path";
-import { writeFile } from "fs/promises";
-import { NextApiRequest } from "next";
 import { put } from "@vercel/blob";
+import { createResponse } from "@/app/lib/utils/prismaUtils";
 
-export const POST = async (req: NextRequest, res: NextResponse) => {
+export const POST = async (req: NextRequest) => {
   const formData = await req.formData();
 
   const file = formData.get("file");
   if (!file) {
-    return NextResponse.json({ error: "No files received." }, { status: 400 });
+    return createResponse(400, { Message: "No files received." });
   }
 
   const fileObj = file as File;
   const filename =  fileObj.name.replaceAll(" ", "_");
   console.log(filename);
   try {
-
     const blob = await put(filename, file, {
       access: 'public',
     });
-    return NextResponse.json({ Message: `Success ${filename}`, status: 200 });
+    return createResponse(200, { Message: `Successfully uploaded ${filename}` });
   } catch (error) {
     console.log("Error occured ", error);
-    return NextResponse.json({ Message: `Failed ${filename}`, status: 500 });
+    return createResponse(500, { Message: `Failed to upload ${filename}` });
   }
 };
 
--- app/ui/Upload.tsx
+++ app/ui/Upload.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { UploadOutlined } from '@ant-design/icons';
-import type { UploadProps } from 'antd';
-import { Button, message, Upload } from 'antd';
-const uploadUrl = `${process.env.NEXT_PUBLIC_VERCEL_URL}/upload`;
+import { UploadOutlined, CheckCircleOutlined, LoadingOutlined, CheckCircleFilled } from '@ant-design/icons';
+import { Button, message, Upload, Spin } from 'antd';
+import type { UploadProps, UploadFile } from 'antd/es/upload/interface';
+
+const uploadUrl: string = `${process.env.NEXT_PUBLIC_API_URL}/upload`;
 
 const props: UploadProps = {
   name: 'file',
-  action: 'http://localhost:3000/api/upload',
+  action: uploadUrl,
   headers: {
     authorization: 'authorization-text',
+  },
+  beforeUpload: (file) => {
+    const isPDF = file.type === 'application/pdf';
+    if (!isPDF) {
+      message.error(`${file.name} is not a PDF file`);
+    }
+    return isPDF || Upload.LIST_IGNORE;
   },
   onChange(info) {
     if (info.file.status !== 'uploading') {
@@ -20,12 +28,45 @@ const props: UploadProps = {
       message.error(`${info.file.name} file upload failed.`);
     }
   },
+  showUploadList: false, // Hide default upload list
 };
 
-const PDFUpload: React.FC = () => (
-  <Upload {...props}>
-    <Button icon={<UploadOutlined />}>Click to Upload</Button>
-  </Upload>
-);
+const PDFUpload: React.FC = () => {
+  const [fileList, setFileList] = React.useState<UploadFile[]>([]);
+  const [isUploading, setIsUploading] = React.useState(false);
+
+  const customProps = {
+    ...props,
+    onChange: (info: any) => {
+      if (info.file.status === 'uploading') {
+        setIsUploading(true); // Set isUploading to true when a file starts uploading
+      } else if (info.file.status === 'done' || info.file.status === 'error') {
+        props?.onChange && props.onChange(info); // Call the original onChange handler if it exists
+        setIsUploading(false);
+      }
+      // Keep only the current file in the fileList
+      setFileList([info.file]);
+    },
+  };
+
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div style={{ marginRight: 16 }}>
+        {fileList.map(file => (
+          <div key={file.uid}>
+            <span className='mr-2'>{file.name}</span>
+          
+            {file.status === 'uploading' ? <Spin indicator={<LoadingOutlined style={{ fontSize: 16 }} spin />}/> : <CheckCircleFilled style={{ color: '#52c41a', fontSize: 16 }} />}
+          </div>
+        ))}
+      </div>
+
+      {/* Upload button */}
+      <Upload {...customProps}>
+        <Button type='primary'  disabled={isUploading} icon={<UploadOutlined />}>Upload to mailbox</Button>
+      </Upload>
+    </div>
+  );
+};
 
 export default PDFUpload;
